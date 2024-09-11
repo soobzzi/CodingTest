4af6ed51917b2bc9b49690f6c6c8bf0677a604ba
soobzzi <ehfdkrkwk101@naver.com>
2024-09-11 18:23:43 +0900

    24.9.11 boj

---
+++ codingTest/src/boj/Jungmin.java
@@ -0,0 +1,20 @@
+package boj;
+
+import java.util.Scanner;
+
+public class Jungmin {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        
+        System.out.println(a+b+c);
+
+        
+		
+	}
+
+}
